aaee5708a06a8a2295c13c723992a552edd8e68d
sgrinenko <hrynenkoserhii@gmail.com>
2020-03-31 15:32:35 +0300

    create isSorted method to check strings order in List<WebElements>

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -3,9 +3,11 @@ import org.junit.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
@@ -16,37 +18,51 @@ public class TestBase {
     private String creds = "admin";
 
     @Before
-    public void setUp(){
+    public void setUp() {
         driver = new FirefoxDriver();
-        wait = new WebDriverWait(driver,3);
+        wait = new WebDriverWait(driver, 3);
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
     }
 
     @After
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
         driver = null;
     }
 
-    protected void loginAsAdmin(){
+    protected void loginAsAdmin() {
         driver.get("http://localhost/litecart/admin/");
         driver.findElement(By.name("username")).sendKeys(creds);
         driver.findElement(By.name("password")).sendKeys(creds);
         driver.findElement(By.name("login")).click();
     }
-    protected void goToStore(){
+
+    protected void goToStore() {
         driver.get("http://localhost/litecart/");
     }
 
-    public boolean isElementPresent(By locator){
+    protected boolean isElementPresent(By locator) {
         try {
             wait.until((WebDriver d) -> d.findElement(locator).isDisplayed());
             return true;
-        }catch (TimeoutException ex){
+        } catch (TimeoutException ex) {
             return false;
         }
     }
-    public boolean areElementsPresent(By locator){
+
+    protected boolean isSorted(List<WebElement> l) {
+        for (int i = 1; i < l.size() - 1; i++) {
+            if (l.get(i).getText().compareTo(l.get(i + 1).getText()) > 0
+                    || l.get(i).getText().compareTo(l.get(i + 1).getText()) == 0 && l.get(i+1).getText()!=null) {
+                System.out.println(l.get(i).getText() + " >" + l.get(i+1).getText()+ "<") ;
+
+                return false;
+            }
+        }
+        return true;
+    }
+
+    protected boolean areElementsPresent(By locator) {
         return driver.findElements(locator).size() > 0;
     }
 
